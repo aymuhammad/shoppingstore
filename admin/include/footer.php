335ab26eb43ac6a6cd751f1ab353791fd376ea40
Auwal Yahaya <auwalyah@yahoo.com>
2022-02-09 12:20:58 +0100

    first commit

---
+++ admin/include/footer.php
@@ -0,0 +1,5 @@
+	<footer class="footer bg-dark">
+      <div class="container">
+        <p class="m-0 text-center text-white">Copyright &copy; Shopping Portal <?php echo date('Y'); ?></p>
+      </div>
+    </footer>
